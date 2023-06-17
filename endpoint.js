e97cb3f871729aef42474622acd884d0e8503a0f
Denizuh <danishs2007@gmail.com>
2023-06-17 11:24:49 +0530

    new file:   config.json 	modified:   endpoint.js 	new file:   fluffyfocus 	modified:   index.html 	modified:   losi.html 	modified:   profile.html
    	modified:   fluffyfocus (modified content)
    server side commit

--- endpoint.js
+++ endpoint.js
@@ -1,3 +1,25 @@
+function clearContentById(id) {
+  var element = document.getElementById(id);
+  if (element) {
+    element.innerHTML = '';
+  }
+}
+
+
+var serverURL;
+
+fetch("config.json")
+  .then(response => response.json())
+  .then(data => {
+    var serverIP = data.server.ip;
+    var serverPort = data.server.port;
+    serverURL = "https://" + serverIP + ":" + serverPort;
+    // ...
+  })
+  .catch(error => {
+    console.error("Error loading configuration file:", error);
+  });
+
 // Set the cookie with a name, value, and expiration time
 function setCookie(name, value, hours) {
     var expires = "";
@@ -47,19 +69,71 @@ $(document).ready(function() {
 
         // Send a POST request to the server
         $.ajax({
-            url: "http://13.233.199.32:9481/",
-            type: "POST",
-            contentType: "application/json",
-            data: JSON.stringify(data),
-            success: function(response) {
-                document.getElementById("create_status").innerHTML = ("Account Created Successfully - Dont forget the password!");
-            },
-            error: function(xhr, status, error) {
-                document.getElementById('create_error-msg').innerHTML = ("Error Occured : " + error);
-            }
-        });
+          url: serverURL,
+          type: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(data),
+          success: function(response) {
+              if (response === "User added successfully") {
+                  clearContentById("create_status")
+                  clearContentById("create_error-msg")
+                  document.getElementById("create_status").innerHTML = "Account Created Successfully - Don't forget the password!";
+                  setTimeout(function() {
+                      location.hash = "login-form";
+                      location.reload();
+                  }, 2000); // 2000 milliseconds = 2 seconds
+              } else {
+                  clearContentById("create_error-msg")
+                  document.getElementById("create_error-msg").innerHTML = "Email or username already exists";
+              }
+          },
+          error: function(xhr, status, error) {
+              clearContentById("create_error-msg")
+              document.getElementById("create_error-msg").innerHTML = "Error Occurred: " + error;
+          }
+      });      
     });
 });
+
+function deleteProfile() {
+  document.getElementById('overlay-delete').style.display = 'flex';
+}
+
+function confirmDelete() {
+  var password = document.getElementById('password-input').value;
+
+  // Create the data object
+  var data = {
+    passphrase: "xv98qds",
+    command: "rem_user",
+    username: localStorage.getItem('username'),
+    password: password
+  };
+
+  // Send a POST request to the server
+  $.ajax({
+    url: serverURL,
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(data),
+    success: function(response) {
+      if (response === "User deleted successfully") {
+        alert("User deleted successfully")
+        showLoggedOutDialog();
+        setTimeout(function() {
+          window.location.href = "index.html";
+      }, 2000);
+      } else if (response === "Incorrect password") {
+        document.getElementById('delete-error-msg').innerHTML = "Incorrect password";
+      } else {
+        document.getElementById('delete-error-msg').innerHTML = "Failed to delete the account";
+      }
+    },
+    error: function(xhr, status, error) {
+      document.getElementById('delete-error-msg').innerHTML = "Error Occurred: " + error;
+    }
+  });
+}
 
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
@@ -71,7 +145,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     var xhr = new XMLHttpRequest();
     
     // Define the request URL
-    var url = 'http://13.233.199.32:9481';
+    var url = serverURL;
   
     // Set the request method to POST
     xhr.open('POST', url);
@@ -94,11 +168,14 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         console.log(response);
         if (response == 200) {
             localStorage.setItem("username", username)
+            clearContentById("create_error-msg")
+            clearContentById('status')
             document.getElementById('status').textContent = ("Login successful Redirecting...");
             setTimeout(function() {
                 window.location.href = "index.html";
             }, 2000);
         } else {
+          clearContentById('error-msg')
           document.getElementById('error-msg').textContent = response;
         }
       }
@@ -106,4 +183,3 @@ document.getElementById('login-form').addEventListener('submit', function(event)
 
     xhr.send(payload);
   });
-  
