c4d7f6d568e1951d9f929e7a2b10c9ca9a7e152d
Denizuh <danishs2007@gmail.com>
2023-06-21 16:51:28 +0530

    added sorting algo 	modified:   filelist.py

--- filelist.py
+++ filelist.py
@@ -4,8 +4,7 @@ import json
 image_path = "images/"  # Replace with the actual path to your image directory
 
 jpeg_files = [f for f in os.listdir(image_path) if f.endswith(".jpg") or f.endswith(".jpeg")]
-
-file_list = [file_name for file_name in jpeg_files]
+sorted_files = sorted(jpeg_files)
 
 with open(f"{image_path}/filelist.json", "w") as file:
-    json.dump(file_list, file)
+    json.dump(sorted_files, file)
